30e3fa55adac9be53e790232416c61c0ff9d4e56
coralie <rey.coralie@free.fr>
2026-05-18 17:24:47 +0200

    html form contact

--- frontend/src/pages/DemandeDemo.jsx
+++ frontend/src/pages/DemandeDemo.jsx
@@ -1,31 +1,68 @@
-import { Link } from 'react-router-dom';
-
 import HeroBg from '../components/HeroBg';
+
+import '../assets/styles/contact.css';
 
 const DemandeDemo = () => {
     return (
-        <main className="main">
+        <main className="main main__contact">
 
             {/* Hero */}
-            <section className="hero" 
-                style={{
-                    height: "calc(100vh - 400px)",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    gap: "3rem"
-                }}>
+            <section className="hero">
                 <HeroBg />
                 <div className="container hero__container">
                     <div className="hero__title">
                         <h1 className='main-title'>
-                            Page <em className='higlight'>en construction</em>
+                            Nous contacter
                         </h1>
-                    </div>
-                    <div className="hero__actions">
-                        <Link className='ui__btn' to="/">Retour à l'accueil</Link>
+                        <p className='subtitle'>
+                        Vous avez une question ou souhaitez en savoir plus sur nos solutions&nbsp;?<br/>
+                        Remplissez le formulaire ci-dessous et nous vous répondrons rapidement.
+                        </p>
                     </div>
                 </div>
+
+                
+            </section>
+
+            <section className="contact__content">
+                <form action="" className='contact__form'>
+                    <p className='form__item--half'>
+                        <label htmlFor="firstName">Prénom*</label>
+                        <input type="text" id="firstName" placeholder="Votre prénom*"/>
+                    </p>
+                    <p className='form__item--half'>
+                        <label htmlFor="lastName">Nom*</label>
+                        <input type="text" id="lastName" placeholder="Votre nom*"/>
+                    </p>
+                    <p className='form__item--half'>
+                        <label htmlFor="email">Email*</label>
+                        <input type="text" id="email" placeholder="Votre email*"/>
+                    </p>
+                    <p className='form__item--half'>
+                        <label htmlFor="phone">Téléphone*</label>
+                        <input type="text" id="phone" placeholder="Votre numéro de téléphone*"/>
+                    </p>
+                    <p className='form__item'>
+                        <label htmlFor="company">Entreprise</label>
+                        <input type="text" id="company" placeholder="Entreprise"/>
+                    </p>
+                    <p className='form__item'>
+                        <label htmlFor="subject">Sujet*</label>
+                        <select id="subject" defaultValue="">
+                            <option value="">-- Sélectionnez un sujet --</option>
+                            <option value="Demande d'information">Demande d'information</option>
+                            <option value="Demande de démo">Demande de démo</option>
+                            <option value="Partenariat">Partenariat</option>
+                            <option value="Support technique">Support technique</option>
+                            <option value="Autre">Autre</option>
+                        </select>
+                    </p>
+                    <p className='form__item'>
+                        <label htmlFor="message">Message*</label>
+                        <textarea type="text" id="message" placeholder="Votre message*" rows="10"></textarea>
+                    </p>
+                    <button type="submit" className='ui__btn form__submit'>Envoyer le message</button>
+                </form>
             </section>
         </main>
     );
