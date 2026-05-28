17885744166fefe858a9fff775294efce274663c
coralie <rey.coralie@free.fr>
2026-05-28 14:57:20 +0200

    ajout page Confidentitalité

--- frontend/src/pages/MentionsLegales.jsx
+++ frontend/src/pages/MentionsLegales.jsx
@@ -18,7 +18,7 @@ const MentionsLegales = () => {
 
             <div className="container">
 
-                <p className="mt mb">
+                <p className="mtXl">
                     Conformément aux dispositions de la loi n°2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site <span className='highlight'>LEGOMNIA</span> l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
                 </p>
                 
