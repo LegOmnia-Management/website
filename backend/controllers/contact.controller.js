d66ef544af7ed9e2baed0c1cf48cf43ddeeaf7db
coralie <rey.coralie@free.fr>
2026-05-29 11:07:42 +0200

    ajout envoi email notif avec chaque demande de contact via le formulaire

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,10 +1,34 @@
 import { Contact } from "../models/index.js";
+import { Resend } from "resend";
 
 /********** CREATE **********/
 const createContact = async (req, res) => {
 
     try {
         const contact = await Contact.create(req.body);
+
+        // Email notif
+        const resend = new Resend(process.env.RESEND_API_KEY);
+        try {
+            await resend.emails.send({
+                from: "onboarding@resend.dev",
+                to: process.env.CONTACT_EMAIL,
+                subject: `Nouveau message :  ${contact.subject}`,
+                html: `
+                    <p><strong>Sujet :</strong> ${contact.subject}</p>
+                    <hr/>
+                    <p><strong>Expéditeur :</strong> ${contact.firstName} ${contact.lastName}</p>
+                    <p><strong>Email :</strong> ${contact.email}</p>
+                    <p><strong>Téléphone :</strong> ${contact.phone}</p>
+                    <p><strong>Entreprise :</strong> ${contact.company}</p>
+                    <p><strong>Message :</strong> ${contact.message}</p>
+                `,
+            });
+        } catch (emailError) {
+            if (process.env.NODE_ENV === "development") {
+                console.error("Erreur envoi email :", emailError);
+            }
+        }
     
         // retour de l'API
         return res.status(201).json({
