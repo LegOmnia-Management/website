7e05a0f315acff034a7e83ebe0d4ff19970921a4
JP <jp.bertaud@legomnia.com>
2026-06-29 16:56:51 +0200

    Refactor server port and remove unused comments
    Updated server port configuration and cleaned up commented code.

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import contactRoutes from "./routes/contact.route.js";
     
 dotenv.config();
 connectDB();
+
 const app = express();
 
 app.use(cors({
@@ -13,26 +14,17 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/hello', (req, res) => {
-    res.json({
-        message: "Hello World !"
-    });
+    res.json({ message: "Hello World !" });
 });
 
 app.use("/api/contact", contactRoutes);
 
-/***************** Pour Vercel *****************/
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT ? Number(process.env.PORT) : 5171;
-    app.listen(PORT, () => {
-        console.log(`API lancée sur http://localhost:${PORT}`);
-    });
-}
-export default app;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(PORT, () => {
+    console.log(`API lancée sur le port : ${PORT}`);
+});
 
-/***************** Générique *****************/
-// const PORT = process.env.PORT ? Number(process.env.PORT) : 5171;
-// app.listen(PORT, () => {
-//     console.log(`API lancée sur le port : ${PORT}`);
-// });
+export default app;
