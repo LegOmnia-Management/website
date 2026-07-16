9a387db0f93c5d92609a5bdaadc861bd5e6af6f6
coralie <rey.coralie@free.fr>
2026-07-16 09:50:19 +0200

    clean files

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL, 'https://website-lemon-gamma-10.vercel.app'],
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 app.use(express.json());
